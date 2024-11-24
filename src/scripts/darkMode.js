9be98f013c78b901e2e7577cfbb7e44a2af1a1ee
nrj022 <0602smj@gmail.com>
2024-11-24 14:56:32 +0100

    add dark mode button & code refactoring

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,9 +1,29 @@
-// check OS Dark Mode
-function checkDarkMode(){
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.setAttribute("color-theme", "dark");
-    } else {
-        document.documentElement.setAttribute("color-theme", "light");
-    }
+// Change the mode of the site
+function updateDarkMode(darkMode) {
+    var baseImgPath = document.getElementById("dark-mode").getAttribute("data-img-path")
+    
+    localStorage.setItem("darkMode", darkMode.toString());
+    document.documentElement.setAttribute("color-theme", darkMode ? "dark" : "light");
+    document.getElementById("dark-mode").src = baseImgPath + (darkMode ? "sun.png" : "moon.png");
 }
-checkDarkMode();
+
+// Initial mode setting of the page
+function setDarkMode() {
+    var darkMode = localStorage.getItem("darkMode");
+
+    // If the value is not in localStorage, set it according to the OS's default settings
+    if(darkMode === null) {
+        darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'true' : 'false';
+    }
+
+    updateDarkMode(darkMode === 'true')
+}
+
+// When the user clicks the Dark Mode button
+function switchDarkMode() {
+    var darkMode = localStorage.getItem("darkMode") === 'true';
+    updateDarkMode(!darkMode)
+}
+
+setDarkMode();
+document.getElementById("dark-mode").addEventListener("click", switchDarkMode)
--- src/scripts/screenWidth.js
+++ src/scripts/screenWidth.js
@@ -1,26 +1,11 @@
-// references from html file
-const pContainer = document.getElementById("product-container");
-
-function checkIsVertical() {
-    if(window.innerWidth < window.innerHeight) {
-        return true
-    } else {
-        return false
-    }
-}
-
+// Change page layout
 function changeArrangement() {
-    if(checkIsVertical()) {
-        pContainer.style.flexDirection = 'column'
-    } else {
-        pContainer.style.flexDirection = 'row'
-    }
+    const pContainer = document.getElementById("product-container");
+    pContainer.style.flexDirection = window.innerWidth < window.innerHeight ? 'column' : 'row' // Check screen orientation and Change direction
 }
 
-// check the size when the screen first loads
+// Check the size when the screen first loads
 changeArrangement()
 
-// detect changes in screen size
-window.addEventListener("resize", () => {
-    changeArrangement()
-})
+// Detect changes in screen size
+window.addEventListener("resize", changeArrangement)
